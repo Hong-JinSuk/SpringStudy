f29e92561b56f0c730a8dd081daef201012cc168
RealTone <realtone98@gmail.com>
2024-03-13 18:38:49 +0900

    start Spring with ApllicationContext

--- SpringCorePrin-Basic/src/main/java/hello/core/AppConfig.java
+++ SpringCorePrin-Basic/src/main/java/hello/core/AppConfig.java
@@ -12,20 +12,26 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration //
 public class AppConfig { // 나의 에플리케이션 전체를 설정 & 구성 한다는 의미
 
+    // Key = memberService 이고 value 는 new MemberServiceImpl(memberRepository()) 로 등록이 되는 느낌
+    @Bean // Spring이 @Bean이 붙어있는 메소드들을 컨테이너에 넣어준다.
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- SpringCorePrin-Basic/src/main/java/hello/core/MemberApp.java
+++ SpringCorePrin-Basic/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // Spring은 모두 ApplicationContext로 시작하여 모든 객체들을 관리한다. (@Bean들)
+        // Annotation(@) 기반으로 만든 메소드들이므로 new An... text 로 생성해줘야 한다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // Appconfig에서 memberService라는 메서드를 가져오고, 그 클래스를 MemberService.class 로 알려준다.
+        // 여기서는 memberService 가 memberService = new MemberServiceImpl(memberRepository()); 가 되는 것이다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
