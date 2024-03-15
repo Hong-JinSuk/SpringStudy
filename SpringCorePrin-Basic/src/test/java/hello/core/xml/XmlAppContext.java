582705afdc521c3c226f776e5a063614e887a322
RealTone <realtone98@gmail.com>
2024-03-15 18:12:32 +0900

    Bean 조회 및 xml 설정 정보 관련

---
+++ SpringCorePrin-Basic/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,21 @@
+package hello.core.xml;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    @DisplayName("xml 기반의 appConfig 설정 정보에 대해 배워보자...")
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
