f6bb8913384213f3e41f1472a9b982f27aae2df0
RealTone <realtone98@gmail.com>
2024-03-21 18:05:06 +0900

    commit HTTPRequestServlet

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,39 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String method = request.getMethod();
+
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " + locale));
+        System.out.println("request.getLocale() = " + request.getLocale());
+        System.out.println();
+
+        System.out.println("[cookie 조회]");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + " : " + cookie.getValue());
+            }
+        }
+
+        System.out.println("[Content 편의 조회]");
+        System.out.println("request.getContentType() = " +
+                request.getContentType());
+        System.out.println("request.getContentLength() = " +
+                request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " +
+                request.getCharacterEncoding());
+    }
+}
