748cf12abc7a5d21ecde227c43b5772bf1a6e0d0
RealTone <realtone98@gmail.com>
2024-04-05 18:57:26 +0900

    login by session & session info, timeout

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -2,12 +2,18 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.PrimitiveIterator;
 
 @Slf4j
 @Controller
@@ -16,6 +22,8 @@ public class HomeController {
 
     private final MemberRepository memberRepository;
 
+    private final SessionManager sessionManager;
+
     //    @GetMapping("/")
     public String home() {
         return "home";
@@ -23,7 +31,7 @@ public class HomeController {
 
     // CookieValue 는 스프링에서 제공하는 쿠키를 받아오는 어노테이션이다.
     // 로그인하지 않은 사람들도 들어오기 때문에, required = false  다.
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
 
         if (memberId == null) {
@@ -36,6 +44,58 @@ public class HomeController {
             return "home";
         }
 
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    //    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        // 세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+        // 로그인
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    //    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        // false 를 넣지 않으면, 로그인하지 않은 사용자에게도 세션을 줘버린다.
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        // 세션 관리자에 저장된 회원 정보 조회
+        Object loginMember = session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    // 세션을 찾아서 loginMember 에 넣어준다.
+    @GetMapping("/")
+    public String homeLoginV4(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
---
+++ login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,6 @@
+package hello.login.web;
+
+// 상수만 쓸 것이므로 생성못하게한다.
+public abstract class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
