8ca47449a57515cefd95ea3f5857e461f2e75fdf
RealTone <realtone98@gmail.com>
2024-04-08 17:43:41 +0900

    spring TypeConverter & Formatter

---
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,26 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        // MyNumber... 에서 formatter 보다 converter 가 우선순위상 우위에 있으므로 주석처리함.
+        // 실제로 MyNumber... 에도 주석처리한 기능이 있기때문에 괜찮음.
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
+    }
+}
