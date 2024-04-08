4bb45c8dc0dc2e37609a6d06bf2640de59b35f6b
RealTone <realtone98@gmail.com>
2024-04-08 11:45:54 +0900

    ExceptionHandler & ControllerAdvice

---
+++ exception/src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -0,0 +1,61 @@
+package hello.exception.api;
+
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class ApiExceptionV3Controller {
+
+    // IllegalArgumentException 을 잡아서 여기서 처리해주는 것이다.
+    // 따라서 예외가 아닌 정상처리되므로 ResponseStatus 로 오류임을 알려줘야 한다.
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e){
+        log.error("[exceptionHandler]",e);
+        return new ErrorResult("BAD",e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e){
+        log.error("[exceptionHandler] ex",e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity(errorResult,HttpStatus.BAD_REQUEST);
+    }
+
+    // Exception 이 최상위 오류이므로, 위에서 처리하지 못한 것들은 Exception 에서 모두 처리된다.
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e){
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
+    @GetMapping("/api3/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+        if (id.equals("bad")) { // 이건 절대로 넘기면 안되는 값이라고 정해놓은 것
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
+
+        return new MemberDto(id, "hello"+id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
